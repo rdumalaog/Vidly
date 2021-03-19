4fea08d401582b0da4452a506ed8b10268ea79dc
Raymond Dumalaog <rdumalaog@capitoltechsolutions.com>
2021-03-19 02:10:20 -0700

    Add project files.

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //customers/customer
+        public ActionResult Customer()
+        {
+            var cust = new List<Customer>()
+            {
+                //new Customer { Name = "John Smith" },
+                //new Customer { Name = "Mary Williams" },
+            };
+            var viewModel = new CustomersViewModel
+            {
+                Customers = cust
+            };
+            return View(viewModel);
+        }
+
+        //customers/customer/{id}
+        public ActionResult Details(int id)
+        {
+            var cust = new Customer();
+            cust.Id = id;
+
+            if (id == 1)
+                cust.Name = "John Smith";
+            else if (id == 2)
+                cust.Name = "Mary Williams";
+            else
+                return HttpNotFound();
+
+            return View(cust);
+        }
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModel;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy{1}", pageIndex, sortBy));
+        }
+
+        // movies/random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!"};
+
+            var customers = new List<Customer>() 
+            { 
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        // movies/edit?id=1
+        public ActionResult Edit(int id)
+        {
+            return Content("id="+ id);
+        }
+
+        // movies/released/2021/03
+        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/"+ month);
+        }
+    }
+}
